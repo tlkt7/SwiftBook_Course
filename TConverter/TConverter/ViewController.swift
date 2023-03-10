e86ae6b648220badbb8300d7e99c1330e31bd05f
Ibrahhim Tlektes <i.tlkt7@gmail.com>
2023-03-10 11:21:09 +0600

    Add files via upload

---
+++ TConverter/TConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Ibrahim on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var celsius: UILabel!
+    @IBOutlet weak var farenheit: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+    }
+}
+
